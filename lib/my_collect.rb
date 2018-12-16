dff989a8874c37dd0561c85fb4e82472da2297a3
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-12-16 01:58:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
 i = 0
 new_array = []
+if array == languages
 while i < array.length
-  yield(array[i].split(" ").first)
+  yield(array[i].upcase)
   i = i + 1
 end
 new_array = array
